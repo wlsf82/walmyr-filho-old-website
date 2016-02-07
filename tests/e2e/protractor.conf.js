b6e24d4701197215dcb563f53975edfd5cd81d02
wlsf82 <wlsf82@gmail.com>
2016-02-07 02:51:27 -0200

    Updated test comment.

--- tests/e2e/protractor.conf.js
+++ tests/e2e/protractor.conf.js
@@ -41,7 +41,7 @@ module.exports.config = {
     browser.driver.controlFlow().execute = function() {
       var args = arguments;
 
-      // queue 200ms wait.
+      // queue 100ms wait.
       origFn.call(browser.driver.controlFlow(), function() {
         return protractor.promise.delayed(100);
       });
