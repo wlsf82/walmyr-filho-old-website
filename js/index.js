47fadbddbeabf45add78bba1756aef27be7595bf
Walmyr Filho <walmyr@appear.in>
2016-10-14 13:09:02 +0200

    Updated index.js and added bundle.js to use appear.in sdk.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Uses rem for size so go ahead and change the font-size of html and see the buttons get bigger (or smaller...) :)
+var AppearIn = window.AppearIn || require('appearin-sdk');
+var appearin = new AppearIn();
+
+appearin.getRandomRoomName().then(function (roomName) {
+    var iframe = document.getElementById("iframe-appear");
+    roomName = 'mentoring' + roomName;
+
+    appearin.addRoomToIframe(iframe, roomName);
+    console.log(roomName);
+});
