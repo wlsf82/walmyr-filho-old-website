3b80ac1c8789e40d6636e4254a97f4b38d73d304
Walmyr Filho <walmyr@appear.in>
2016-12-31 00:08:32 +0100

    update 404

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var images = [
+  './img/404_1.png',
+  './img/404_2.png'
+];
+
+style404ImageRandomly();
+
+function style404ImageRandomly() {
+  return Math.random() > 0.5 ? styleImage(0) : styleImage(1);
+}
+
+function styleImage(image) {
+  document.querySelector('#page-not-found')
+    .innerHTML = "<img src='" + images[image] + "' alt='Página não encontrada' width='700' height='600'/>";
+}
