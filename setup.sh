26087e69026b50aa27aa3bed10f248096266cc8e
Walmyr <wlsf82@gmail.com>
2018-08-01 12:19:57 +0200

    add files needed for backstopjs tests

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export DOMAIN=file:///Users/walmyr/www/walmyr-filho-personal-brand
+export PROJECT_ID='Walmyr Filho old website'
+
+echo "Domain:" $DOMAIN
+echo "Project ID:" $PROJECT_ID
---
+++ test/relativePaths.js
@@ -0,0 +1,11 @@
+module.exports = [
+  '/index.html',
+  '/consultoria.html',
+  '/desenvolvimento.html',
+  '/es-index.html',
+  '/es-consultoria.html',
+  '/desarrollo.html',
+  '/en-index.html',
+  '/consultancy.html',
+  '/development.html',
+];
