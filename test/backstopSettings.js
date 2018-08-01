d90f4f936d9ccb1d43619d921f307f95cea3d922
Walmyr <wlsf82@gmail.com>
2018-08-01 20:15:21 +0200

    update project with backstopjs tests

---
+++ test/backstopSettings.js
@@ -0,0 +1,35 @@
+const relativePaths = require('./relativePaths.js');
+const scenarios = [];
+
+relativePaths.map(relativePath => {
+  scenarios.push({
+    label: relativePath,
+    url: `${process.env.DOMAIN}${relativePath}`,
+    delay: 3000,
+    requireSameDimensions: false
+  });
+});
+
+module.exports = {
+  id: process.env.PROJECT_ID,
+  viewports: [
+    {
+      name: "desktop",
+      width: 1280,
+      height: 1024
+    },
+  ],
+  scenarios,
+  paths: {
+    bitmaps_reference: "test/backstop_data/bitmaps_reference",
+    bitmaps_test: "test/backstop_data/bitmaps_test",
+    html_report: "test/backstop_data/html_report"
+  },
+  report: ["browser"],
+  engine: "puppeteer",
+  engineOptions: {
+    args: ["--no-sandbox"]
+  },
+  asyncCaptureLimit: 5,
+  asyncCompareLimit: 50
+};
---
+++ test/relativePaths.js
@@ -0,0 +1,11 @@
+module.exports = [
+  '/index.html',
+  '/es-index.html',
+  '/en-index.html',
+  '/consultoria.html',
+  '/es-consultoria.html',
+  '/consultancy.html',
+  '/desenvolvimento.html',
+  '/desarrollo.html',
+  '/development.html',
+];
