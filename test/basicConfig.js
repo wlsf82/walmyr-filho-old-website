22222c11776c562f752b12d8f6807eb0af249434
Walmyr <wlsf82@gmail.com>
2018-08-08 17:00:22 +0200

    update vrt based on changes no backstop-config project

---
+++ test/basicConfig.js
@@ -0,0 +1,25 @@
+const baseUrl = "file:///Users/walmyr/www/walmyr-filho-personal-brand/src/html";
+const projectId = "Walmyr Filho old website";
+
+const relativeUrls =[
+  "/index.html",
+  "/es-index.html",
+  "/en-index.html",
+  "/consultoria.html",
+  "/es-consultoria.html",
+  "/consultancy.html",
+  "/desenvolvimento.html",
+  "/desarrollo.html",
+  "/development.html",
+];
+
+const viewports = [
+  "desktop",
+];
+
+module.exports = {
+  baseUrl,
+  projectId,
+  relativeUrls,
+  viewports,
+};
